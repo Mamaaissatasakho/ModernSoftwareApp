5f6108af65770e6314784fe7b86b050724bbbf11
MamaAissataSakho <aicha.sakho06@gmail.com>
2025-09-09 21:38:03 +0200

    Init: console app + tests xUnit

---
+++ ModernSoftwareApp.Tests/CalculatorTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+using ModernSoftwareApp;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+}
